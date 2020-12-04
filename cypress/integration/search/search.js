c1818c485427903943354068db231cb07d73aba0
shashikala <shashikala@intimetec.com>
2020-12-04 16:51:04 +0530

    d

--- cypress/integration/search/search.js
+++
@@ -1,7 +0,0 @@
-import { When, Then } from 'cypress-cucumber-preprocessor/steps';
-
-import HomePage from '../../../pages/homePage';
-
-When(/^Press 'Search'$/, () => {
-  HomePage.pressSearch();
-});
